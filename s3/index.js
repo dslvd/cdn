71ea64848a4d109ff5e5584726d6275ee1ed6f52
dslvd <80934874+dslvd@users.noreply.github.com>
2023-04-11 18:01:46 +0800

    Update index.js

--- s3/index.js
+++ s3/index.js
@@ -4,8 +4,8 @@ import fs from 'fs';
 const client = new S3Client({
   region: process.env.AWS_BUCKET_REGION,
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY,
-    secretAccessKey: process.env.AWS_SECRET_KEY,
+    accessKeyId: process.env.AWS_ACCESS,
+    secretAccessKey: process.env.AWS_SECRET,
   },
 
 });
